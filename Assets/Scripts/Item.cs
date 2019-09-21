d5fcc88b8a386e71485ea13d0bf7271b4689b52c
Oliver Savastano Becker <oliverbecker@usp.br>
2019-09-21 18:32:03 -0300

    Item adicionado

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+    //public string name;
+    //public Sprite sprite;
+    //public GameObject machine;
+    //private BoxCollider2D collider;
+
+    // Start is called before the first frame update
+    void Start() {
+        //collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Destroy(gameObject);
+    }
+}
